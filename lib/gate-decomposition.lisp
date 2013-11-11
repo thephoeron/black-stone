83eb948bccb6e493f0e9757381c00adc0e9e1152
the Phoeron <thephoeron@me.com>
2013-11-10 19:27:52 -0500

    Adding additional skeleton files

---
+++ lib/gate-decomposition.lisp
@@ -0,0 +1,8 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*- file: gate-decomposition.lisp
+
+;;;; Copyright (c) 2013 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; See LICENSE for additional information.
+
+(in-package :black-stone)
+
+;; EOF
---
+++ packages.lisp
@@ -0,0 +1,17 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*- file: packages.lisp
+
+;;;; Copyright (c) 2013 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; See LICENSE for additional information.
+
+(in-package :cl-user)
+
+(defpackage #:black-stone
+    (:nicknames #:qcl)
+    (:use :cl)
+    (:export #:*black-stone-version*))
+
+;; see asdf system definition
+(defvar black-stone:*black-stone-version*
+  #.black-stone-asd::*black-stone-version*)
+
+;; EOF
