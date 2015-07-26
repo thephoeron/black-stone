949b58b68aec84e43c21b0c539764af2aaf7f0c4
the Phoeron <sysop@thephoeron.com>
2015-07-26 18:12:09 -0400

    add Qubit generation test

--- t/black-stone.lisp
+++ t/black-stone.lisp
@@ -9,7 +9,7 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :bit-smasher)' in your Lisp.
 
-(plan 1)
+(plan 2)
 
 (deftest sanity-check
   (pass "PROVE is loaded and ready to go.")
@@ -24,6 +24,23 @@
   (is (mod (+ 10 2) 10)
       2
       "Modulus: (mod (+ 10 2) 10) => 2."))
+
+(deftest qubits
+  (is-type (make-instance 'qubit)
+           'qubit
+           "Qubit created successfully.")
+  (is (black-stone::qubit-state (make-instance 'qubit))
+      1.0
+      "The State of the Qubit is 1.0.")
+  (is (black-stone::alpha (make-instance 'qubit))
+      0.0
+      "Alpha: 0.0")
+  (is (black-stone::beta (make-instance 'qubit))
+      1.0
+      "Beta: 1.0")
+  (is (black-stone::pure-state (make-instance 'qubit))
+      #C(0.0 0.7071067932881648)
+      "Pure Qubit State: ~#C(0.0 0.71)"))
 
 (run-test-all)
 
