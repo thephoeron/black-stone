09c5b736aa17f3fcf0f66844024909a5b02e86ba
the Phoeron <thephoeron@users.noreply.github.com>
2015-10-09 14:49:34 -0400

    Update qdata.lisp

--- qclambda/qdata.lisp
+++ qclambda/qdata.lisp
@@ -1,5 +1,5 @@
 ;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*-
-;;;; file: qdata.lisp
+;;;; file: qclambda/qdata.lisp
 
 ;;;; Copyright (c) 2013--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
