9393247192c0909a330bf654354fedeed863ae3f
the Phoeron <thephoeron@users.noreply.github.com>
2015-10-09 14:49:13 -0400

    Update printing.lisp

--- qclambda/printing.lisp
+++ qclambda/printing.lisp
@@ -1,5 +1,5 @@
 ;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*-
-;;;; file: printing.lisp
+;;;; file: qclambda/printing.lisp
 
 ;;;; Copyright (c) 2013--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
