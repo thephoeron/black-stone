b9cf2fe06d13d3b7d64429473538205cae887cbd
the Phoeron <thephoeron@users.noreply.github.com>
2015-10-10 10:33:09 -0400

    Update labels.lisp

--- qclambda/labels.lisp
+++ qclambda/labels.lisp
@@ -1,6 +1,7 @@
-;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*- file: labels.lisp
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*-
+;;;; file: qclambda/labels.lisp
 
-;;;; Copyright (c) 2013--2014 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; Copyright (c) 2013--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
 
 (in-package :black-stone)
