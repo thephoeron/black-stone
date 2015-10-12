ae49996a59e163b98160304bc7dafe44fb9f56f2
the Phoeron <thephoeron@users.noreply.github.com>
2015-10-12 10:52:21 -0400

    Update arithmetic.lisp

--- lib/arithmetic.lisp
+++ lib/arithmetic.lisp
@@ -1,6 +1,7 @@
-;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*- file: arithmetic.lisp
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*-
+;;;; file: lib/arithmetic.lisp
 
-;;;; Copyright (c) 2013--2014 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; Copyright (c) 2013--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
 
 (in-package :black-stone)
