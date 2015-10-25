3f792ce1ffb49fbbd90a8b258e666436e04b3fec
the Phoeron <thephoeron@users.noreply.github.com>
2015-10-25 11:02:29 -0400

    Update simulation-clifford.lisp

--- lib/simulation-clifford.lisp
+++ lib/simulation-clifford.lisp
@@ -1,6 +1,7 @@
-;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*- file: simulation-clifford.lisp
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE; Base: 10 -*-
+;;;; file: lib/simulation-clifford.lisp
 
-;;;; Copyright (c) 2013--2014 "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; Copyright (c) 2013--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
 ;;;; See LICENSE for additional information.
 
 (in-package :black-stone)
