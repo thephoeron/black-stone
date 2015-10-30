1b2e9d6d890b4d49cdcb5f4e25e0a58d28ada5ed
the Phoeron <thephoeron@users.noreply.github.com>
2015-10-30 09:10:02 -0400

    Update t/black-stone.lisp

--- t/black-stone.lisp
+++ t/black-stone.lisp
@@ -1,4 +1,8 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: BLACK-STONE-TEST; Base: 10 -*-
 ;;;; file: t/black-stone.lisp
+
+;;;; Copyright (c) 2013--2015, "the Phoeron" Colin J.E. Lupton <//thephoeron.com>
+;;;; See LICENSE for additional information.
 
 (in-package :cl-user)
 
